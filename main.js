99689c5508b9abcc56420e2f5e2ee2c66ea42b10
Alex <muirkat138@gmail.com>
2021-09-14 16:37:30 -0600

    js data

---
+++ main.js
@@ -0,0 +1,14 @@
+// DATA
+let clickCount = 0 
+let height = 120
+let width = 100
+let inflationRate = 20
+let maxsize = 300
+let highestPopCount = 0
+let currentPopCount = 0
+let gameLength = 10000
+let clockId = 0
+let timeRemaining = 0
+let currentPlayer = {}
+let currentColor = "red"
+let possibleColors = ["red", "green", "blue", "purple", "pink"]
